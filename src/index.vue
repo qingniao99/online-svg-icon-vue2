907444bbc7e5a795569813222dd4d6c8b3ed2f1d
梦不觉 <huangjianqiang126@163.com>
2022-07-05 15:15:13 +0800

    update 1.1.3

--- src/index.vue
+++ src/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="online-svg-wrap">
-      <div class="icon" :style="genStyle"></div>
+  <div class="online-svg-wrap" ref="wrap">
+      <div class="icon" :style="genStyle"  v-if="purecss"></div>
+      <object class="iconob" type="image/svg+xml" :data="database64" v-if="!purecss"></object>
   </div>
 </template>
 
@@ -9,23 +10,30 @@ import axios from "axios";
 export default {
   name: 'onlineSvgIcon',
   props: {
+    purecss: {
+      type: Boolean,
+      default: true
+    },
     url: String
   },
   data() {
     return {
       svg: '',
+      database64: '',
+      observerwh: null,
     }
   },
   computed: {
     genStyle() {
-        if (!this.svg) return {};
+        if (!this.svg || !this.purecss) return {};
         const mode = this.svg.includes('currentColor')
           ? 'mask'
           : 'background-img'
 
         const uri = `url("data:image/svg+xml;utf8,${this.encodeSvg(this.svg)}")`
+
         // monochrome
-        if (mode === 'mask') {
+        if (mode === 'mask' && this.purecss) {
           return {
             'background-color': 'currentColor',
             height: '1em',
@@ -56,13 +64,28 @@ export default {
       this.getSvg();
     }
   },
-  mounted() {
-    this.getSvg();  
+  async mounted() {
+    await this.getSvg(); 
+    this.observerwh = new MutationObserver(this.pwd);
+      this.observerwh.observe(this.$refs.wrap, {
+          attributes: true,
+      });
   },
   methods: {
+    pwd() {
+      this.up64vg();
+    },
     async getSvg() {
       const res = await axios.get(this.url);
+      console.log(res, 888);
       this.svg = res.data;
+      this.up64vg();
+    },
+    up64vg() {
+      const color = window.getComputedStyle(this.$refs.wrap).color;
+      let tepsvg = this.svg;
+      tepsvg = tepsvg.replace('currentColor', color);
+      this.database64 = `data:image/svg+xml;base64,${window.btoa(tepsvg)}`;
     },
     encodeSvg(svgString) {
         return svgString.replace('<svg',(~svgString.indexOf('xmlns')?'<svg':'<svg xmlns="http://www.w3.org/2000/svg"'))
@@ -80,10 +103,18 @@ export default {
         .replace(/\s+/g,' ');
       }
   },
+  destroyed() {
+    this.observerwh && this.observerwh.disconnect();
+  },
 }
 </script>
 <style scoped>
 .online-svg-wrap {
   display: inline-block;
+}
+.iconob {
+  display: block;
+  width: 1em;
+  height: 1em;
 }
 </style>
